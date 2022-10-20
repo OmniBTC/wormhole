0913a15ee104429de83b37d5b94d32750ea7e5cb
AAweidai <dw1253464613@gmail.com>
2022-10-20 14:26:30 +0800

    Update rpc service

--- relayer/spy_relayer/src/helpers/mongoHelper.ts
+++ relayer/spy_relayer/src/helpers/mongoHelper.ts
@@ -1,9 +1,9 @@
-import {MongoClient} from "mongodb";
-import {getListenerEnvironment} from "../configureEnv";
-import {getScopedLogger} from "./logHelper";
-import {parseVAA, VAA} from "./serdeHelper";
-import {ChainId, uint8ArrayToHex} from "@certusone/wormhole-sdk";
-import {BigNumber} from "ethers";
+import { MongoClient } from "mongodb";
+import { getListenerEnvironment } from "../configureEnv";
+import { getScopedLogger } from "./logHelper";
+import { parseTransfer, parseTransferWithPayload, parseVAA, VAA } from "./serdeHelper";
+import { ChainId, hexToUint8Array, uint8ArrayToHex } from "@certusone/wormhole-sdk";
+import { BigNumber } from "ethers";
 
 const logger = getScopedLogger(["Mongo"]);
 
@@ -14,161 +14,223 @@ export const mongoClient = new MongoClient(mongoUrl);
 export const wormholeDB = mongoClient.db("Wormhole");
 
 export interface VAAStorage extends VAA {
-    hexString: string;
+  hexString: string;
+  toChain: ChainId;
+  to: string;
 }
 
 export interface RelayerDstGas {
-    chainId: ChainId;
-    vaaLength: number;
-    dstSwapLength: number;
-    estimateGas: BigInt;
-    estimateGasPrice: BigInt;
-    actualGas: BigNumber;
-    actualGasPrice: BigNumber;
+  chainId: ChainId;
+  vaaLength: number;
+  dstSwapLength: number;
+  estimateGas: BigInt;
+  estimateGasPrice: BigInt;
+  actualGas: BigNumber;
+  actualGasPrice: BigNumber;
 }
 
 export const vaaCol = wormholeDB.collection<VAAStorage>("VAAStorage");
 export const dstGasCol = wormholeDB.collection<RelayerDstGas>("RelayerDstGas");
 
 export async function addVaaInMongo(rawVaa: Uint8Array): Promise<VAA | null> {
+  try {
+    const vaa = await parseVAA(rawVaa);
+
+    let transferPayload;
+    let toChain;
+    let to;
     try {
-        const vaa = await parseVAA(rawVaa);
-        await vaaCol.updateOne(
-            {
-                sequence: vaa.sequence,
-                emitterChainId: vaa.emitterChainId,
-                emitterAddress: vaa.emitterAddress,
-            },
-            {
-                $set: {
-                    version: vaa.version,
-                    consistencyLevel: vaa.consistencyLevel,
-                    emitterAddress: vaa.emitterAddress,
-                    emitterChainId: vaa.emitterChainId,
-                    guardianSetIndex: vaa.guardianSetIndex,
-                    hash: vaa.hash,
-                    nonce: vaa.nonce,
-                    payload: vaa.payload,
-                    sequence: vaa.sequence,
-                    signatures: vaa.signatures,
-                    timestamp: vaa.timestamp,
-                    hexString: uint8ArrayToHex(rawVaa),
-                },
-            },
-            {upsert: true}
+      transferPayload = await parseTransferWithPayload(
+        hexToUint8Array(vaa.payload.replace("0x", ""))
+      );
+      toChain = transferPayload.toChain;
+      to = transferPayload.to;
+    } catch (_) {
+      try {
+        transferPayload = await parseTransfer(
+          hexToUint8Array(vaa.payload.replace("0x", ""))
         );
-        logger.info(
-            "Add vaa to mongo, sequence: " +
-            vaa.sequence +
-            " emitterAddress: " +
-            vaa.emitterAddress +
-            " emitterChainId: " +
-            vaa.emitterChainId +
-            " hash: " +
-            vaa.hash
-        );
-        return vaa;
-    } catch (e) {
-        logger.error("Add vaa to mongo fail: ", e);
+        toChain = transferPayload.toChain;
+        to = transferPayload.to;
+      } catch (_) {
+      }
     }
-    return null;
+
+    await vaaCol.updateOne(
+      {
+        sequence: vaa.sequence,
+        emitterChainId: vaa.emitterChainId,
+        emitterAddress: vaa.emitterAddress
+      },
+      {
+        $set: {
+          version: vaa.version,
+          consistencyLevel: vaa.consistencyLevel,
+          emitterAddress: vaa.emitterAddress,
+          emitterChainId: vaa.emitterChainId,
+          guardianSetIndex: vaa.guardianSetIndex,
+          hash: vaa.hash,
+          nonce: vaa.nonce,
+          payload: vaa.payload,
+          sequence: vaa.sequence,
+          signatures: vaa.signatures,
+          timestamp: vaa.timestamp,
+          hexString: uint8ArrayToHex(rawVaa),
+          toChain: toChain,
+          to: to
+        }
+      },
+      { upsert: true }
+    );
+    logger.info(
+      "Add vaa to mongo, sequence: " +
+      vaa.sequence +
+      " emitterAddress: " +
+      vaa.emitterAddress +
+      " emitterChainId: " +
+      vaa.emitterChainId +
+      " hash: " +
+      vaa.hash
+    );
+    return vaa;
+  } catch (e) {
+    logger.error("Add vaa to mongo fail: ", e);
+  }
+  return null;
 }
 
 export async function findVaaInMongo(
-    sequence: string,
-    emitterChainId?: ChainId,
-    emitterAddress?: string
+  sequence: string,
+  emitterChainId?: ChainId,
+  emitterAddress?: string
 ): Promise<VAAStorage | null> {
-    try {
-        // logger.debug(
-        //   `Find vaa --> sequence:[${sequence}], emitterChainId:[${emitterChainId}] emitterAddress:[${emitterAddress}]`
-        // );
-        if (emitterChainId && emitterAddress) {
-            return await vaaCol.findOne<VAAStorage>({
-                sequence: sequence,
-                emitterChainId: emitterChainId,
-                emitterAddress: emitterAddress,
-            });
-        } else if (emitterChainId) {
-            return await vaaCol.findOne<VAAStorage>({
-                sequence: sequence,
-                emitterChainId: emitterChainId
-            });
-        } else {
-            return await vaaCol.findOne<VAAStorage>({
-                sequence: sequence
-            })
-        }
-    } catch (e) {
-        logger.error(
-            "Find " +
-            emitterChainId +
-            ", " +
-            emitterAddress +
-            ", " +
-            sequence +
-            " fail: ",
-            e
-        );
+  try {
+    if (emitterChainId && emitterAddress) {
+      return await vaaCol.findOne<VAAStorage>({
+        sequence: sequence,
+        emitterChainId: emitterChainId,
+        emitterAddress: emitterAddress
+      });
+    } else if (emitterChainId) {
+      return await vaaCol.findOne<VAAStorage>({
+        sequence: sequence,
+        emitterChainId: emitterChainId
+      });
+    } else {
+      return await vaaCol.findOne<VAAStorage>({
+        sequence: sequence
+      });
     }
-    return null;
+  } catch (e) {
+    logger.error(
+      "Find " +
+      emitterChainId +
+      ", " +
+      emitterAddress +
+      ", " +
+      sequence +
+      " fail: ",
+      e
+    );
+  }
+  return null;
+}
+
+export async function findVaaByToInMongo(
+  toChain: ChainId,
+  to?: string,
+  count?: number
+): Promise<VAAStorage[] | null> {
+  try {
+    if (!count) {
+      count = 10;
+    }
+    let cursor;
+    if (to) {
+      cursor = vaaCol.find<VAAStorage>({
+          toChain: toChain,
+          to: to
+        }, { sort: { timestamp: -1 } }
+      );
+    } else {
+      cursor = vaaCol.find<VAAStorage>({
+          toChain: toChain
+        }, { sort: { timestamp: -1 } }
+      );
+    }
+    let output: VAAStorage[] = [];
+    await cursor.limit(count).forEach((d) => {
+      output.push(d);
+    });
+    return output;
+  } catch (e) {
+    logger.error(
+      "Find toChain:" +
+      toChain +
+      ", to:" +
+      to +
+      ", fail: ",
+      e
+    );
+  }
+  return null;
 }
 
 export async function addDstGasInMongo(
-    relayerDstGas: RelayerDstGas
+  relayerDstGas: RelayerDstGas
 ): Promise<RelayerDstGas | null> {
-    if (
-        relayerDstGas.estimateGas === BigInt(0) ||
-        relayerDstGas.estimateGasPrice === BigInt(0)
-    ) {
-        logger.warn("Estimated relayer dst gas or gas price is 0.");
-        return null;
-    }
-    try {
-        await dstGasCol.insertOne({
-            chainId: relayerDstGas.chainId,
-            vaaLength: relayerDstGas.vaaLength,
-            dstSwapLength: relayerDstGas.dstSwapLength,
-            estimateGas: relayerDstGas.estimateGas,
-            estimateGasPrice: relayerDstGas.estimateGasPrice,
-            actualGas: relayerDstGas.actualGas,
-            actualGasPrice: relayerDstGas.actualGasPrice,
-        });
-        logger.info(
-            "Add dst gas to mongo, chainId: " +
-            relayerDstGas.chainId +
-            " vaaLength: " +
-            relayerDstGas.vaaLength +
-            " dstSwapLength: " +
-            relayerDstGas.dstSwapLength +
-            " estimateGas: " +
-            relayerDstGas.estimateGas +
-            " estimateGasPrice: " +
-            relayerDstGas.estimateGasPrice +
-            " actualGas: " +
-            relayerDstGas.actualGas +
-            " actualGasPrice: " +
-            relayerDstGas.actualGasPrice
-        );
-    } catch (e) {
-        logger.error("Add relayer dst gas to mongo fail: ", e);
-    }
+  if (
+    relayerDstGas.estimateGas === BigInt(0) ||
+    relayerDstGas.estimateGasPrice === BigInt(0)
+  ) {
+    logger.warn("Estimated relayer dst gas or gas price is 0.");
     return null;
+  }
+  try {
+    await dstGasCol.insertOne({
+      chainId: relayerDstGas.chainId,
+      vaaLength: relayerDstGas.vaaLength,
+      dstSwapLength: relayerDstGas.dstSwapLength,
+      estimateGas: relayerDstGas.estimateGas,
+      estimateGasPrice: relayerDstGas.estimateGasPrice,
+      actualGas: relayerDstGas.actualGas,
+      actualGasPrice: relayerDstGas.actualGasPrice
+    });
+    logger.info(
+      "Add dst gas to mongo, chainId: " +
+      relayerDstGas.chainId +
+      " vaaLength: " +
+      relayerDstGas.vaaLength +
+      " dstSwapLength: " +
+      relayerDstGas.dstSwapLength +
+      " estimateGas: " +
+      relayerDstGas.estimateGas +
+      " estimateGasPrice: " +
+      relayerDstGas.estimateGasPrice +
+      " actualGas: " +
+      relayerDstGas.actualGas +
+      " actualGasPrice: " +
+      relayerDstGas.actualGasPrice
+    );
+  } catch (e) {
+    logger.error("Add relayer dst gas to mongo fail: ", e);
+  }
+  return null;
 }
 
 export async function findDstGasInMongo(
-    chainId: ChainId,
-    dstSwapLength: number
+  chainId: ChainId,
+  dstSwapLength: number
 ): Promise<RelayerDstGas[] | null> {
-    try {
-        return await dstGasCol
-            .find<RelayerDstGas>({
-                chainId: chainId,
-                dstSwapLength: dstSwapLength
-            })
-            .toArray();
-    } catch (e) {
-        logger.error("Find " + chainId + " dst gas fail: ", e);
-    }
-    return null;
+  try {
+    return await dstGasCol
+      .find<RelayerDstGas>({
+        chainId: chainId,
+        dstSwapLength: dstSwapLength
+      })
+      .toArray();
+  } catch (e) {
+    logger.error("Find " + chainId + " dst gas fail: ", e);
+  }
+  return null;
 }
--- relayer/spy_relayer/src/listener/vaa_req.ts
+++ relayer/spy_relayer/src/listener/vaa_req.ts
@@ -1,86 +1,115 @@
 import * as express from "express";
-import {findDstGasInMongo, findVaaInMongo} from "../helpers/mongoHelper";
-import {leftPaddingAddress} from "../helpers/serdeHelper";
-import {getScopedLogger} from "../helpers/logHelper";
-import {BigNumber} from "ethers";
+import { findDstGasInMongo, findVaaByToInMongo, findVaaInMongo } from "../helpers/mongoHelper";
+import { getScopedLogger } from "../helpers/logHelper";
+import { BigNumber } from "ethers";
 
 const wormholeApp = express();
 
 export async function wormholeAppInit() {
-    const logger = getScopedLogger(["wormholeAppInit"]);
-    wormholeApp.use(express.json());
+  const logger = getScopedLogger(["wormholeAppInit"]);
+  wormholeApp.use(express.json());
 
-    let rpc_port = process.env.RPC_PORT ? parseInt(process.env.RPC_PORT) : 5066;
-    wormholeApp.listen(rpc_port, "localhost", () => {
-        logger.info(`Wormhole Rpc Service start up: localhost:${rpc_port}`);
-    });
+  let rpc_port = process.env.RPC_PORT ? parseInt(process.env.RPC_PORT) : 5066;
+  wormholeApp.listen(rpc_port, "localhost", () => {
+    logger.info(`Wormhole Rpc Service start up: localhost:${rpc_port}`);
+  });
 
-    wormholeApp.post("*", async function (req, res) {
-        try {
-            if (req.body.method == "GetSignedVAA") {
-                logger.info(
-                    `WormholeApp process request: method: ${req.body.method}, params: ${req.body.params}`
-                );
+  wormholeApp.post("*", async function(req, res) {
+    try {
+      if (req.body.method == "GetSignedVAA") {
+        logger.info(
+          `WormholeApp process request: method: ${req.body.method}, params: ${req.body.params}`
+        );
 
-                let response;
-                if (req.body.params.length === 0) {
-                    response = "Required params: sequence [str], Optional params: chainid [number]";
-                } else if (req.body.params.length === 1) {
-                    let result = await findVaaInMongo(
-                        req.body.params[0].toString()
-                    );
-                    response = JSON.stringify(result);
-                } else {
-                    let result = await findVaaInMongo(
-                        req.body.params[0].toString(),
-                        req.body.params[1]
-                    );
-                    response = JSON.stringify(result);
-                }
-
-                res.send(response);
-            } else if (req.body.method == "GetDstGas") {
-                logger.info(
-                    `WormholeApp process request: method: ${req.body.method}, params: ${req.body.params}`
-                );
-                let response;
-                if (req.body.params.length === 2) {
-                    let result = await findDstGasInMongo(req.body.params[0], req.body.params[1]);
-                    if (result && result[0]) {
-                        if (result[1]) {
-                          for (let i = 1; i < result.length; i++) {
-                            result[0].vaaLength += result[i].vaaLength;
-                            result[0].estimateGas = BigNumber.from(result[0].estimateGas).add(BigNumber.from(result[i].estimateGas)).toBigInt();
-                            result[0].estimateGasPrice = BigNumber.from(result[0].estimateGasPrice).add(BigNumber.from(result[i].estimateGasPrice)).toBigInt();
-                            result[0].actualGas = BigNumber.from(result[0].actualGas).add(result[i].actualGas);
-                            result[0].actualGasPrice = BigNumber.from(result[0].actualGasPrice).add(result[i].actualGasPrice);
-                          }
-                        }
-
-                        let avgVaaLength = Math.floor(result[0].vaaLength / result.length);
-                        let avgEstimateGas = BigNumber.from(result[0].estimateGas).div(result.length).toString();
-                        let avgEstimateGasPrice = BigNumber.from(result[0].estimateGasPrice).div(result.length).toString();
-                        let avgActualGas = result[0].actualGas.div(result.length).toString();
-                        let avgActualGasPrice = result[0].actualGasPrice.div(result.length).toString();
-                        response = JSON.stringify({
-                            "VaaLength": avgVaaLength,
-                            "EstimateGas": avgEstimateGas,
-                            "EstimateGasPrice": avgEstimateGasPrice,
-                            "ActualGas": avgActualGas,
-                            "ActualGasPrice": avgActualGasPrice,
-                        });
-                    } else {
-                        response = `Not exist dst gas at chain ${req.body.params[0]} for swap length ${req.body.params[1]}`;
-                    }
-                } else {
-                    response = "Required params: chainId [number], dstSwapLength [number]";
-                }
-                res.send(response);
-            } else {
-                res.send(`Not found method: ${req.body.method}`);
-            }
-        } catch (e) {
-            logger.info(`WormholeApp process request fail: ${e}`);
+        let response;
+        if (req.body.params.length === 0) {
+          response = "Required params: sequence [str], Optional params: chainid [number]";
+        } else if (req.body.params.length === 1) {
+          let result = await findVaaInMongo(
+            req.body.params[0].toString()
+          );
+          response = JSON.stringify(result);
+        } else if (req.body.params.length === 2) {
+          let result = await findVaaInMongo(
+            req.body.params[0].toString(),
+            req.body.params[1]
+          );
+          response = JSON.stringify(result);
+        } else {
+          let result = await findVaaInMongo(
+            req.body.params[0].toString(),
+            req.body.params[1],
+            req.body.params[2].toString()
+          );
+          response = JSON.stringify(result);
         }
-    });
+        res.send(response);
+      }else if (req.body.method == "GetSignedVAAByTo") {
+        logger.info(
+          `WormholeApp process request: method: ${req.body.method}, params: ${req.body.params}`
+        );
+
+        let response;
+        if (req.body.params.length === 0) {
+          response = "Required params: toCHain [number], Optional params: to [string]";
+        } else if (req.body.params.length === 1) {
+          let result = await findVaaByToInMongo(
+            req.body.params[0]
+          );
+          response = JSON.stringify(result);
+        } else {
+          let result = await findVaaInMongo(
+            req.body.params[0],
+            req.body.params[1].toString()
+          );
+          response = JSON.stringify(result);
+        }
+        res.send(response);
+      }
+
+
+      else if (req.body.method == "GetDstGas") {
+        logger.info(
+          `WormholeApp process request: method: ${req.body.method}, params: ${req.body.params}`
+        );
+        let response;
+        if (req.body.params.length === 2) {
+          let result = await findDstGasInMongo(req.body.params[0], req.body.params[1]);
+          if (result && result[0]) {
+            if (result[1]) {
+              for (let i = 1; i < result.length; i++) {
+                result[0].vaaLength += result[i].vaaLength;
+                result[0].estimateGas = BigNumber.from(result[0].estimateGas).add(BigNumber.from(result[i].estimateGas)).toBigInt();
+                result[0].estimateGasPrice = BigNumber.from(result[0].estimateGasPrice).add(BigNumber.from(result[i].estimateGasPrice)).toBigInt();
+                result[0].actualGas = BigNumber.from(result[0].actualGas).add(result[i].actualGas);
+                result[0].actualGasPrice = BigNumber.from(result[0].actualGasPrice).add(result[i].actualGasPrice);
+              }
+            }
+
+            let avgVaaLength = Math.floor(result[0].vaaLength / result.length);
+            let avgEstimateGas = BigNumber.from(result[0].estimateGas).div(result.length).toString();
+            let avgEstimateGasPrice = BigNumber.from(result[0].estimateGasPrice).div(result.length).toString();
+            let avgActualGas = result[0].actualGas.div(result.length).toString();
+            let avgActualGasPrice = result[0].actualGasPrice.div(result.length).toString();
+            response = JSON.stringify({
+              "VaaLength": avgVaaLength,
+              "EstimateGas": avgEstimateGas,
+              "EstimateGasPrice": avgEstimateGasPrice,
+              "ActualGas": avgActualGas,
+              "ActualGasPrice": avgActualGasPrice
+            });
+          } else {
+            response = `Not exist dst gas at chain ${req.body.params[0]} for swap length ${req.body.params[1]}`;
+          }
+        } else {
+          response = "Required params: chainId [number], dstSwapLength [number]";
+        }
+        res.send(response);
+      } else {
+        res.send(`Not found method: ${req.body.method}`);
+      }
+    } catch (e) {
+      logger.info(`WormholeApp process request fail: ${e}`);
+    }
+  });
 }
